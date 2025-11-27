769da2c0bd55737c410efa9bd3dd6ca43e28a2f0
hungc <hungc@MANHUNG>
2025-11-27 14:57:17 +0700

    first

---
+++ src/java/murach/sql/SQLGatewayServlet.java
@@ -0,0 +1,94 @@
+package murach.sql;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+import javax.servlet.annotation.WebServlet;
+@WebServlet("/sqlGateway")
+public class SQLGatewayServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+        try {
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    String url = "jdbc:mysql://localhost:3306/murach?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
+    String user = "murach_user";
+    String pass = "sesame";
+
+    Connection conn = DriverManager.getConnection(url, user, pass);
+    System.out.println("Kết nối MySQL thành công!");
+
+    conn.close();
+} catch (Exception e) {
+    e.printStackTrace();
+}
+
+
+        String sqlStatement = request.getParameter("sqlStatement");
+        String sqlResult = "";
+
+        try {
+            // load driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // connect database
+            String dbURL = "jdbc:mysql://localhost:3306/murach?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
+            String username = "murach_user";
+            String password = "sesame";
+
+            Connection connection = DriverManager.getConnection(
+                    dbURL, username, password);
+
+            Statement statement = connection.createStatement();
+
+            sqlStatement = sqlStatement.trim();
+
+            if (sqlStatement.length() >= 6) {
+                String sqlType = sqlStatement.substring(0, 6);
+
+                if (sqlType.equalsIgnoreCase("select")) {
+                    ResultSet resultSet = statement.executeQuery(sqlStatement);
+                    sqlResult = SQLUtil.getHtmlTable(resultSet);
+                    resultSet.close();
+                } else {
+                    int i = statement.executeUpdate(sqlStatement);
+
+                    if (i == 0) { 
+                        sqlResult =
+                                "<p>The statement executed successfully.</p>";
+                    } else {
+                        sqlResult =
+                                "<p>The statement executed successfully.<br>"
+                                + i + " row(s) affected.</p>";
+                    }
+                }
+            }
+
+            statement.close();
+            connection.close();
+
+        } catch (ClassNotFoundException e) {
+            sqlResult = "<p>Error loading the database driver:<br>"
+                    + e.getMessage() + "</p>";
+        } catch (SQLException e) {
+            sqlResult = "<p>Error executing the SQL statement:<br>"
+                    + e.getMessage() + "</p>";
+        }
+
+        // save to session
+        HttpSession session = request.getSession();
+        session.setAttribute("sqlResult", sqlResult);
+        session.setAttribute("sqlStatement", sqlStatement);
+
+        getServletContext()
+                .getRequestDispatcher("/index.jsp")
+                .forward(request, response);
+    }
+}
